5f7d293968eaf300dd2bdf4a3f9ce0f47ce1686d
Shivanshu Pandey <pandeyshivanshu2005@gmail.com>
2025-05-28 16:36:26 +0530

    basic

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,23 +8,24 @@ app.get('/', (c) => {
 
 
 app.post('/api/v1/signup' ,(c) =>{
+  return c.text("signUp Route")
 
 })
 
 app.post('/api/v1/signin' , (c)=>{
-
+  return c.text("signIn Route")
 })
 
 app.post('/api/v1/blog' , (c) => {
 
 })
 
-app.put('/api/v1/blog' ,(c) =>{
+app.put('/api/v1/blog/:id' ,(c) =>{
 
 })
 
 app.get('/api/v1/blog' , (c)=>{
-  
+
 })
 
 export default app
