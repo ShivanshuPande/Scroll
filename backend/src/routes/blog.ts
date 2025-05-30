08b789d26cd90a365ff5b4d4754010198edb56fe
Shivanshu Pandey <pandeyshivanshu2005@gmail.com>
2025-05-30 14:53:38 +0530

    applied the schema detaisl

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -2,6 +2,7 @@ import { Hono } from "hono";
 import { verify } from "hono/jwt";
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import { blogSchema, updateblogSchema } from "@fable07/medium-common";
 
 export const blogRouter = new Hono<{
     Bindings : {
@@ -48,23 +49,41 @@ blogRouter.post('/' ,async (c) => {
     const prisma = new PrismaClient({
     datasourceUrl :c.env.DATABASE_URL
     }).$extends(withAccelerate())
+    
+    try{
+        const authorId = c.get("authorId")
 
-    const authorId = c.get("authorId")
+        const payload = await c.req.json()
 
-    const payload = await c.req.json()
+        const {success} = blogSchema.safeParse(payload)
 
-    const blog =  await prisma.post.create({
-        data :{
-            title : payload.title , 
-            content  : payload.content , 
-            authorId : authorId
+        if(!success){
+            c.status(400)
+            return c.json({
+                error : "not the valid inputs for the blog"
+            })
         }
-    })
+
+        const blog =  await prisma.post.create({
+            data :{
+                title : payload.title , 
+                content  : payload.content , 
+                authorId : authorId
+            }
+        })
 
 
-    return c.json({
-        blogId : blog
-    })
+        return c.json({
+            blogId : blog
+        })
+    }catch(e){
+        return c.json({
+            error:"Error while posting the data"
+        })
+    }
+
+    
+
 })
 
 blogRouter.put('/' ,async(c) =>{
@@ -72,22 +91,34 @@ blogRouter.put('/' ,async(c) =>{
     datasourceUrl :c.env.DATABASE_URL
     }).$extends(withAccelerate())
 
-    const payload = await c.req.json()
+    try {
+        const payload = await c.req.json()
 
-    const blog =  await prisma.post.update({
-        where:{
-            id : payload.id
-        } ,
-        data :{
-            title : payload.title , 
-            content  : payload.content
+        const {success} = updateblogSchema.safeParse(payload);
+
+        if (!success){
+            return c.json({
+                error : "enter valid inputs all updating the blog"
+            })
         }
-    })
+        const blog =  await prisma.post.update({
+            where:{
+                id : payload.id
+            } ,
+            data :{
+                title : payload.title , 
+                content  : payload.content
+            }
+        })
 
 
-    return c.json({
-        blogId : blog
-    })
+        return c.json({
+            blogId : blog
+        })
+    }catch(e){
+        error : "error while updating the blog"
+    }
+    
 })
 
 //pagination --make 9 blogs to be returned on the first page load, more of them only after the user scrolls
