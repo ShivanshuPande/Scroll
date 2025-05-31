3961a3e65a5c0c890ca7a58f22db7481bf204d9e
Shivanshu Pandey <pandeyshivanshu2005@gmail.com>
2025-05-31 17:58:20 +0530

    made the signUp page

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,6 +1,12 @@
-import { Link } from "react-router-dom"
+import { useState } from "react"
+import { Link, redirectDocument } from "react-router-dom"
+
 
 export const Auth = ()=>{
+
+
+    const [email , setEmail] = useState("")
+    const [password , setPassword] = useState("")
     return(
         
         <div className="flex justify-center flex-col">
@@ -18,8 +24,37 @@ export const Auth = ()=>{
                 </div>
             </div>
 
+            <br></br>
+            <br></br>
+            <div className="pb-30"> 
+                <InputBox label="Email" placeholder="Please Enter the email " onChange={(e)=>{setEmail(e.target.value)}}/>
+                <br></br>
+                <InputBox label="Password" placeholder="Please Enter the Password " onChange={(e)=>{setPassword(e.target.value)}}/>
+
+                <div className="pt-8 flex justify-center">
+                    <Button Label="Log In" onClick={InputHandler}/>
+                </div>
+            </div>
         </div>
     )
+}
+
+interface ButtonTypes {
+    Label : string
+    onClick : (e : React.ChangeEvent<HTMLInputElement>)=> void
+}
+
+function Button({Label , onClick} : ButtonTypes) {
+
+    return(
+        <>
+
+            <button onClick={onClick} type="button" className="text-white bg-black 
+           font-medium rounded-lg text-sm px-8 py-2.5 me-2 mb-2
+           ">{Label}</button>
+        </>
+    )
+
 }
 
 interface InputBoxType {
@@ -30,14 +65,18 @@ interface InputBoxType {
 
 function InputBox({label , placeholder , onChange} :  InputBoxType){
     return  <div>
-                <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{label}</label>
-                <input onChange={onChange} type="text" id="first_name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
-                 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400
-                  dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={placeholder} required />
+                <label  className="font-outfit block mb-2 text-sm font-medium text-gray-900">{label}</label>
+                <input onChange={onChange} type="text" id="first_name" className="font-outfit bg-gray-50 border-2 border-gray-300 text-gray-900 text-sm rounded-lg
+                 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-black dark:placeholder-gray-400
+                  dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={placeholder} required />
             </div>
 }
 
-function InputHandler {
-    
+function InputHandler() {
+
+    console.log()
+    //need to make post to the backend that connects and register the details of the User
+
+
 }
 
--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -1,7 +1,7 @@
 export const Quote = ()=>{
     return (
         <>
-        <div className="bg-orange-50 h-screen flex justify-center items-center font-bold text-2xl pl-5 pb-10 font-playfair text-shadow-lg " >
+        <div className="bg-orange-50 h-screen flex justify-center items-center font-bold text-2xl pl-5 pb-10 font-playfair" >
             Blogging is a powerful way to share ideas, connect with others, and inspire meaningful change. It allows you to express your unique perspective, tell your story, and build a community around your passions.
 
         </div>
