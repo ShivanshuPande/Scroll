d9755a091a7eae8bd317f0120bb0066ae352e27a
Shivanshu Pandey <pandeyshivanshu2005@gmail.com>
2025-06-01 11:34:43 +0530

    a lot of basic and quality changes

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,80 +1,96 @@
-import { useState } from "react"
-import { Link } from "react-router-dom"
+        import { useState } from "react"
+        import { Link } from "react-router-dom"
+        import type { SignInInput } from "@fable07/medium-common"
+
+        interface AuthProps { 
+            type : "signin" | "signup"
+        }
 
 
-export const Auth = ()=>{
+        export const Auth = ({type} : AuthProps)=>{
 
+            const [userInputs , setuserInputs ] = useState<SignInInput>({
+                email : "" ,
+                password : ""
+            })
 
-    const [email , setEmail] = useState("")
-    const [password , setPassword] = useState("")
-    return(
-        
-        <div className="flex justify-center flex-col">
-            <div className="font-outfit pt-10">
-                <div className="text-4xl">
-                    Create  a Account
-                </div>
-                <div className="flex flex-row">
-                    <div className="pl-14 text-base ">
-                    Already a User? 
+            const  InputHandler =  () =>{
+                console.log(userInputs)
+                setuserInputs({
+                    email:"" ,
+                    password : ""
+                })
+            //need to make post to the backend that connects and register the details of the User
+            }
+            return(<>
+                {/* <pre>{JSON.stringify(userInputs, null ,2 )}</pre> */}   
+                <div className="flex justify-center flex-col">
+                    <div className="font-outfit pt-10">
+                        <div className="flex justify-center  text-4xl">
+                            {type === "signup" ? "Create an Account" : "   Welcome Back"}
+                        </div>
+                        <div className=" flex justify-center flex-row">
+                            <div className="flex justify-center text-base ">
+                            Already a User? 
+                            </div>
+                            <div className="underline text-base pl-2">
+                            <Link to={"/signin"}>Log In.</Link>
+                            </div>
+                        </div>
                     </div>
-                    <div className="underline text-base pl-2">
-                    <Link to={"/signin"}>Log In.</Link>
+
+                    <br></br>
+                    <br></br>
+                    <div className="pb-30"> 
+                        <InputBox value={userInputs.email} type="text" label="Email" placeholder="Please Enter the email " onChange={(e)=>{setuserInputs({...userInputs , email : e.target.value})}}/>
+                        <br></br>
+                        <InputBox value={userInputs.password} type="password" label="Password" placeholder="Please Enter the Password " onChange={(e)=>{setuserInputs({...userInputs , password : e.target.value})}}/>
+
+                        <div className="pt-8 flex justify-center">
+                            <Button Label="Log In" onClick={InputHandler}/>
+                        </div>
                     </div>
                 </div>
-            </div>
+                </>
+            )
+        }
 
-            <br></br>
-            <br></br>
-            <div className="pb-30"> 
-                <InputBox label="Email" placeholder="Please Enter the email " onChange={(e)=>{setEmail(e.target.value)}}/>
-                <br></br>
-                <InputBox label="Password" placeholder="Please Enter the Password " onChange={(e)=>{setPassword(e.target.value)}}/>
+        interface ButtonTypes {
+            Label : string
+            onClick : ()=> void
+        }
 
-                <div className="pt-8 flex justify-center">
-                    <Button Label="Log In" onClick={InputHandler}/>
-                </div>
-            </div>
-        </div>
-    )
-}
+        function Button({Label , onClick} : ButtonTypes) {
 
-interface ButtonTypes {
-    Label : string
-    onClick : ()=> void
-}
+            return(
+                    <button onClick={onClick} type="button" className="text-white bg-black cursor-pointer
+                font-medium rounded-lg text-sm px-8 py-2.5 me-2 mb-2 active:bg-gray-800
+                ">{Label}</button>
+                
+            )
 
-function Button({Label , onClick} : ButtonTypes) {
+        }
 
-    return(
-            <button onClick={onClick} type="button" className="text-white bg-black 
-           font-medium rounded-lg text-sm px-8 py-2.5 me-2 mb-2
-           ">{Label}</button>
-        
-    )
+        interface InputBoxType {
+            label :string , 
+            placeholder : string,
+            onChange : (e : React.ChangeEvent<HTMLInputElement>) =>void
+            type : "text" | "password"
+            value : string
+        }
 
-}
+        function InputBox({label , placeholder , onChange , type , value} :  InputBoxType){
+            return  <div>
+                        <label  className="font-outfit block mb-2 text-sm font-medium text-gray-900">{label}</label>
+                        <input 
+                        value={value}
+                        onChange={onChange}
+                        type={type}
+                        className="cursor-text font-outfit bg-gray-50 border-2 border-gray-300 text-gray-900 text-sm rounded-lg
+                        block w-full p-2.5 dark:bg-white dark:border-black dark:placeholder-gray-400"
+                        placeholder={placeholder}
+                        required />
+                    </div>
+        }
 
-interface InputBoxType {
-    label :string , 
-    placeholder : string,
-    onChange : (e : React.ChangeEvent<HTMLInputElement>) =>void
-}
-
-function InputBox({label , placeholder , onChange} :  InputBoxType){
-    return  <div>
-                <label  className="font-outfit block mb-2 text-sm font-medium text-gray-900">{label}</label>
-                <input onChange={onChange} type="text"  className="font-outfit bg-gray-50 border-2 border-gray-300 text-gray-900 text-sm rounded-lg
-                 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-black dark:placeholder-gray-400
-                  dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder={placeholder} required />
-            </div>
-}
-
-function InputHandler() {
-
-    console.log()
-    //need to make post to the backend that connects and register the details of the User
-
-
-}
 
--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -6,7 +6,7 @@ export const SignUp = () => {
     <div className="flex justify-center items-center min-h-screen px-20 py-10">
       <div className="grid grid-cols-1 md:grid-cols-2 items-center w-full h-full  border ">
         <div className="bg-stone-170  flex items-center justify-center">
-            <Auth/>
+            <Auth type="signup" />
         </div>
         <div className="flex items-center justify-center">
           <Quote />
