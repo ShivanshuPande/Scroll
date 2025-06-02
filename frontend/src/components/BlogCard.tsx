48ce2af81f1f72a08658c97575d1e77f8081ac84
Shivanshu Pandey <pandeyshivanshu2005@gmail.com>
2025-06-02 17:12:49 +0530

    created the publish button

--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -12,7 +12,7 @@ export const BlogCard =({authorName , title ,content , publishDate} : BlogProps)
                 <div className=" md:h-40 hover:border-black h-50 w-200 pl-10 pr-10 pt-4 border-2 border-gray-500 m-2 bg-fuchsia-50 rounded-xl">
                 <div className="">
                         <div className="flex">      
-                                <Avatar name={authorName}/>
+                                <Avatar fontSize={"base"} size={28} name={authorName}/>
                                 <div className="flex justify-center font-outfit pl-4 font-light">{authorName.slice(0,8)}</div>
                                 <div className="pl-4 font-outfit font-extralight text-sm pt-1 ">{publishDate}</div>
                         </div>
@@ -30,12 +30,19 @@ export const BlogCard =({authorName , title ,content , publishDate} : BlogProps)
 
 interface AvatarProps {
   name: String;
+  size: number;
+  fontSize : String;
 }
 
-export const Avatar: React.FC<AvatarProps> = ({ name }) => {
+export const Avatar: React.FC<AvatarProps> = ({ name  , size , fontSize} ) => {
   return (
-    <div className="relative inline-flex items-center justify-center w-6 h-6 overflow-hidden bg-orange-300 rounded-full">
-      <span className="font-medium font-outfit text-white">
+    <div className={"relative inline-flex items-center justify-center overflow-hidden bg-orange-300 rounded-full"} style={
+        {
+            width : size ,
+            height :size
+        }
+    }>
+      <span className={`text-${fontSize} font-outfit text-white`}>
         {name?.charAt(0).toUpperCase()}
       </span>
     </div>
--- frontend/src/components/BlogHeader.tsx
+++ frontend/src/components/BlogHeader.tsx
@@ -1,6 +1,26 @@
 import { Avatar } from "./BlogCard"
 
+interface ButtonProps {
+    onClick:()=> void   
+}
 
+
+function Button({onClick} : ButtonProps){
+    return (
+    <span className="flex items-center bg-orange-200 rounded-3xl">
+        <button onClick={onClick} className=" flex items-center pl-5 pr-4 ">
+            <span className="pr-2 text-xl  font-outfit">Publish</span>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-5">
+            <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
+            </svg>
+        </button>
+    </span>
+    )
+}
+
+function temp (){
+    console.log("the button is clicked")
+}
 
 export const BlogHeader=()=>{
     return (
@@ -10,15 +30,14 @@ export const BlogHeader=()=>{
                 <div className="font-outfit font-medium text-5xl pl-7 ">
                     Scroll
                 </div>
-                <div>
-                    <button>
-                        hello
-                    </button>
-                </div>
-                <div className="pr-7">
-                    <Avatar name="Shivanshu Pandey"/>
-                </div>
+                <div className="flex">
+                    <Button onClick={temp}/>
+                        <span className="ml-12 pr-4">
+                            <Avatar fontSize={"2xl"} size={50} name="Shivanshu Pandey"/>
+                        </span>
+                    </div>
             </div>
         </div>
         )
 } 
+
