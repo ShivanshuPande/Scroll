5334e687a74e51349ac8e1b104d947b285f10dce
Shivanshu Pandey <pandeyshivanshu2005@gmail.com>
2025-06-02 11:11:53 +0530

    blogcard component

--- frontend/src/components/BlogCard.tsx
+++ frontend/src/components/BlogCard.tsx
@@ -1,9 +1,19 @@
-export const BlogCard =() => {
-    return (
+interface BlogProps {
+    authorName : String
+    title : String
+    content : String
+    publishDate : String
+}
 
-        <>
-            hello from the Blog Card
-        </>
+export const BlogCard =({authorName , title ,content , publishDate} : BlogProps) => {
+    return (
+        <div>
+            <div>{authorName}</div>
+            <div>{title}</div>
+            <div>{content.slice(0,70) +"..."}</div>
+            <div>{publishDate}</div>
+            <div>${Math.ceil(content.length /100)} minutes</div>
+        </div>
     )
 }
 
--- frontend/src/pages/Blogs.tsx
+++ frontend/src/pages/Blogs.tsx
@@ -3,7 +3,8 @@ import { BlogCard } from "../components/BlogCard"
 export const Blogs =()=>{
     return (
         <> 
-            <BlogCard/>
+            <BlogCard title="Hello There" content="This Human vs AI narrative is just nothing but a business 
+            thing , so that much people can carry out there agenda" authorName="Shivanshu Pandey" publishDate="19.07.2005"/>
             hello there , from the blogs page
         </>
     )
