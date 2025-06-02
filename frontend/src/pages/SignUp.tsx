ac70fef45a020834bbf62f109bda4a683eeb163e
Shivanshu Pandey <pandeyshivanshu2005@gmail.com>
2025-06-02 15:07:07 +0530

    fixed the avatar component

--- frontend/src/pages/SignUp.tsx
+++ frontend/src/pages/SignUp.tsx
@@ -1,11 +1,13 @@
 import { Auth } from "../components/Auth";
 import { Quote } from "../components/Quote";
 
+
 export const SignUp = () => {
   return (
     <div className="flex justify-center items-center min-h-screen">
       <div className="grid grid-cols-1 md:grid-cols-2 items-center w-full h-full ">
         <div className="bg-stone-170  flex items-center justify-center pt-30">
+
             <Auth type="signup" />
         </div>
         <div className="flex items-center justify-center">
